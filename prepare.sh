c946dab42dd824b19e95a9d643a1cf1ce15a6560
Darren Strash <darrenstrash@users.noreply.github.com>
2023-11-28 02:21:02 +0000

    install -> myinstall

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ echo "Downloading ncurses 6.2"
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.2.tar.gz
 tar -zxf ncurses-6.2.tar.gz
 cd ncurses-6.2
-mkdir install
-./configure --prefix=`realpath ./install`
+mkdir myinstall
+./configure --prefix=`realpath ./myinstall`
 make
 make install
